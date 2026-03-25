145df3860b06135ed1cac6950a2eb19f283e8614
Saborit <saborit-lope@insa-toulouse.fr>
2026-03-25 22:09:24 +0100

    refactor: moved handler logic to handler.c

--- src/handler.c
+++ src/handler.c
@@ -1,9 +1,90 @@
 #include "handler.h"
+#include "http.h"
 
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)x
+#define FUNUSED __attribute__((unused))
+
+static int send_response(int client_fd, const http_response *r) FUNUSED;
+static void ok_response(http_response *r, const char *body);
+static void method_not_allowed(http_response *r) FUNUSED;
+static void not_found(http_response *r) FUNUSED;
+
 void get_default_handler(const http_request *request, const int client_fd) {
-    (void)request;
-    write(client_fd, "HTTP/1.1 200 OK\r\nContent-Length: 3\r\nConnection: close\r\n\r\nok\n",
-          60);
+    UNUSED(request);
+
+    http_response r;
+    ok_response(&r, "ok buddy\n");
+    send_response(client_fd, &r);
 }
+
+static void ok_response(http_response *r, const char *body) {
+    r->status_code = 200;
+    r->reason = "OK";
+    r->content_type = "text/plain";
+    r->body = body;
+    r->body_length = strlen(r->body);
+}
+
+static void not_found(http_response *r) {
+    r->status_code = 404;
+    r->reason = "Not Found";
+    r->content_type = NULL;
+
+    r->body = NULL;
+    r->body_length = 0;
+}
+
+static void method_not_allowed(http_response *r) {
+    r->status_code = 405;
+    r->reason = "Method Not Allowed";
+    r->content_type = NULL;
+
+    r->body = NULL;
+    r->body_length = 0;
+}
+
+static int send_response(int client_fd, const http_response *r) {
+    char header[1024];
+    size_t len = 0;
+
+    len += snprintf(header + len, sizeof(header) - len, "HTTP/1.1 %d %s\r\n",
+                    r->status_code, r->reason);
+
+    if (r->content_type) {
+        len += snprintf(header + len, sizeof(header) - len,
+                        "Content-Type: %s\r\n", r->content_type);
+    }
+
+    if (r->body && r->body_length > 0) {
+        len += snprintf(header + len, sizeof(header) - len,
+                        "Content-Length: %zu\r\n", r->body_length);
+    }
+
+    len +=
+        snprintf(header + len, sizeof(header) - len, "Connection: close\r\n");
+
+    len += snprintf(header + len, sizeof(header) - len, "\r\n");
+
+    if (len >= sizeof(header)) {
+        return -1;
+    }
+
+    if (write(client_fd, header, len) < 0) {
+        perror("write");
+        return -1;
+    }
+
+    if (r->body && r->body_length > 0) {
+        if (write(client_fd, r->body, r->body_length) < 0) {
+            perror("write");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
--- src/http.c
+++ src/http.c
@@ -1,4 +1,5 @@
 #include "http.h"
+#include "router.h"
 
 #include <stddef.h>
 #include <stdio.h>
@@ -6,12 +7,7 @@
 #include <unistd.h>
 
 static int parse_request_line(const char *buffer, http_request *request);
-static int send_response(int client_fd, const http_response *r);
-static void ok_response(http_response *r, const char *body);
-static void method_not_allowed(http_response *r);
 static http_method str_to_method(const char *buffer);
-static void not_found(http_response *r);
-// static void send_bad_request();
 
 int http_handle_client(int client_fd) {
     http_request req;
@@ -29,19 +25,7 @@ int http_handle_client(int client_fd) {
         return -1;
     }
 
-    if (req.method == HTTP_GET) {
-        http_response r;
-        ok_response(&r, "tysmtysm");
-        send_response(client_fd, &r);
-    } else if (req.method == HTTP_UNKNOWN) {
-        http_response r;
-        method_not_allowed(&r);
-        send_response(client_fd, &r);
-    } else if (req.method == HTTP_PUT) {
-        http_response r;
-        not_found(&r);
-        send_response(client_fd, &r);
-    }
+    router_dispatch(&req, client_fd);
 
     return 0;
 }
@@ -69,75 +53,8 @@ static http_method str_to_method(const char *buffer) {
     if (!buffer) {
         return HTTP_UNKNOWN;
     }
-    if (strcmp(buffer, "GET")) {
+    if (!strcmp(buffer, "GET")) {
         return HTTP_GET;
     }
     return HTTP_UNKNOWN;
-}
-
-static int send_response(int client_fd, const http_response *r) {
-    char header[1024];
-    size_t len = 0;
-
-    len += snprintf(header + len, sizeof(header) - len, "HTTP/1.1 %d %s\r\n",
-                    r->status_code, r->reason);
-
-    if (r->content_type) {
-        len += snprintf(header + len, sizeof(header) - len,
-                        "Content-Type: %s\r\n", r->content_type);
-    }
-
-    if (r->body && r->body_length > 0) {
-        len += snprintf(header + len, sizeof(header) - len,
-                        "Content-Length: %zu\r\n", r->body_length);
-    }
-
-    len +=
-        snprintf(header + len, sizeof(header) - len, "Connection: close\r\n");
-
-    len += snprintf(header + len, sizeof(header) - len, "\r\n");
-
-    if (len >= sizeof(header)) {
-        return -1;
-    }
-
-    if (write(client_fd, header, len) < 0) {
-        perror("write");
-        return -1;
-    }
-
-    if (r->body && r->body_length > 0) {
-        if (write(client_fd, r->body, r->body_length) < 0) {
-            perror("write");
-            return -1;
-        }
-    }
-
-    return 0;
-}
-
-static void ok_response(http_response *r, const char *body) {
-    r->status_code = 200;
-    r->reason = "OK";
-    r->content_type = "text/plain";
-    r->body = body;
-    r->body_length = strlen(r->body);
-}
-
-static void not_found(http_response *r) {
-    r->status_code = 404;
-    r->reason = "Not Found";
-    r->content_type = NULL;
-
-    r->body = NULL;
-    r->body_length = 0;
-}
-
-static void method_not_allowed(http_response *r) {
-    r->status_code = 405;
-    r->reason = "Method Not Allowed";
-    r->content_type = NULL;
-
-    r->body = NULL;
-    r->body_length = 0;
 }
